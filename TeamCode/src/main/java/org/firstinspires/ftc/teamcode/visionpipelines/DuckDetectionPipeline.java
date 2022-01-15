0398f1bb38f0033b6d94990865ddbd405cc03391
FTC4924 <nrrobotics@gmail.com>
2022-01-14 20:23:34 -0500

    Changed the driver station viewport to render in three color channels rather than greyscale.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visionpipelines/DuckDetectionPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visionpipelines/DuckDetectionPipeline.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.visionpipelines;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
@@ -13,6 +14,7 @@ public class DuckDetectionPipeline extends OpenCvPipeline
     public static int leftMean;
     public static int centerMean;
     public static int rightMean;
+    private static Scalar maskedChannels;
 
     Mat YCrCb = new Mat();
 
@@ -47,6 +49,13 @@ public class DuckDetectionPipeline extends OpenCvPipeline
     @Override
     public void init(Mat firstFrame) {
         inputToCb(firstFrame);
+        if (COLOR_CHANNEL == 0) {
+            maskedChannels = new Scalar(1, 0, 0);
+        } else if (COLOR_CHANNEL == 1) {
+            maskedChannels = new Scalar(0, 1, 0);
+        } else {
+            maskedChannels = new Scalar(0, 0, 1);
+        }
     }
 
     @Override
@@ -56,7 +65,9 @@ public class DuckDetectionPipeline extends OpenCvPipeline
         maxAverage();
 
         Mat outputFrame = new Mat();
-        Core.extractChannel(YCrCb, outputFrame, COLOR_CHANNEL);
+        Core.multiply(YCrCb, maskedChannels, outputFrame);
+
+        Imgproc.cvtColor(outputFrame, outputFrame, Imgproc.COLOR_YCrCb2RGB);
 
         Imgproc.rectangle(outputFrame, REGION_A, GREEN, 2);
         Imgproc.rectangle(outputFrame, REGION_B, GREEN, 2);
